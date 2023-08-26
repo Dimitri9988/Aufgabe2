311a84fea68f4ac87188cc0dc607340ce3124b7b
Dimitri9988 <dimitri.steiner@ict.csbe.ch>
2023-08-26 11:35:55 +0200

    fd

--- immutable_objects.js
+++ immutable_objects.js
@@ -10,6 +10,14 @@ const beterUserName2 = {
     ...beterUserName,
     age : 17,
 }
+const beterUserName3 = {
+    ...beterUserName2,
+    age : 18.08-2006,
+}
+const { age, ...newbeterUserName} = beterUserName3;
+
 
 beterUserName
 beterUserName2
+beterUserName3
+newbeterUserName
