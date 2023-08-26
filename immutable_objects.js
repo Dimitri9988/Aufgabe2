b661b43d05d2f456a325f03e4aa1beb15765bb34
Dimitri9988 <dimitri.steiner@ict.csbe.ch>
2023-08-26 11:19:05 +0200

    Aufgabe2 d

--- immutable_objects.js
+++ immutable_objects.js
@@ -1,1 +1,15 @@
-const username = "Dimitri Steiner";
+const userName = "Dimitri9988";
+
+const beterUserName = {
+    firstName : "Dimitri",
+    lastName : "Steiner",
+    username : "Dimitri9988",
+};
+
+const beterUserName2 = {
+    ...beterUserName,
+    age : 17,
+}
+
+beterUserName
+beterUserName2
